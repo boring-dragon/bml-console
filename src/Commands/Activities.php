11cc07a92638c29f4f8a65e6d4b62dd7e43b07a7
Mohamed Jinas <j@live.mv>
2020-09-13 01:12:39 +0500

    Create Activities.php

---
+++ src/Commands/Activities.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jinas\BMLConsole\Commands;
+
+use Jinas\BMLConsole\Helpers\BML;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+
+class Activities
+{
+    public function handle(BML $bml, OutputInterface $output)
+    {
+        $table = new Table($output);
+        $table->setHeaders(['Type', 'Date', 'Contact', 'Amount', 'Remarks', 'Status']);
+
+        foreach ($bml->GetActivities() as $activities) {
+            $rows[] = [
+                $activities["type"], $activities["datetime"], $activities["creditName"], $activities["formattedAmount"], $activities["message"],
+                ($activities["status"] == 'Failed') ? "<fire>Failed</fire>" : "<info>" . $activities["status"] . "</info>"
+            ];
+        }
+
+        $table->setRows($rows);
+        $table->render();
+    }
+}
