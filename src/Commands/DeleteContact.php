dcf8dc9a1f60ad1e34348ca8f27b3a630647bfef
Mohamed Jinas <j@live.mv>
2020-09-22 22:40:59 +0500

    Added contacts autocomplete for delete contact

--- src/Commands/DeleteContact.php
+++ src/Commands/DeleteContact.php
@@ -7,14 +7,20 @@ use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Jinas\BMLConsole\Helpers\Arr;
 
 class DeleteContact
 {
+    use Arr;
+
     public function handle(BML $bml, OutputInterface $output, InputInterface $input)
     {
         $question = new QuestionHelper();
 
+        $contacts = $bml->GetContacts();
+
         $alias = $question->ask($input, $output, (new Question("<question>Whats the Alias name of the account? </question> \n"))
+            ->setAutocompleterValues($this->array_pluck($contacts, 'alias'))
             ->setValidator(function ($value) {
                 if (trim($value) == '') {
                     throw new \Exception('Alias cannot be empty');
@@ -23,7 +29,6 @@ class DeleteContact
                 return $value;
             }));
 
-        $contacts = $bml->GetContacts();
         $contact_key = array_search($alias, array_column($contacts, 'alias'));
         $bml->DeleteContact($contacts[$contact_key]['id']);
 
